c190d3429efb36154b0111ebcf4b9b2ce13f110f
Daniel Semple <dsemple.isu@gmail.com>
2021-06-19 17:33:28 -0500

    working on adding routes, started the navigation component which will be available with links at the top of every screen except the splash screen. Putting the navigation component at the top over every component currently, but believe there has to be a better way, since I'm repeating code.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { CharactersComponent } from './characters/characters.component';
 
 const routes: Routes = [
   { path: "main", component: MainPageComponent },
   { path: "splash", component: SplashScreenComponent },
+  { path: "characters", component: CharactersComponent },
   { path: "", redirectTo: "/splash", pathMatch: "full" }, //Sets the default page to direct to the splash screen page.
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { CharactersComponent } from './characters/characters.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    SplashScreenComponent
+    SplashScreenComponent,
+    CharactersComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
