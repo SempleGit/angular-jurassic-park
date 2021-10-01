c8a9b05a3c664f3915afe4c9da5847858513cd3d
Daniel Semple <dsemple.isu@gmail.com>
2021-09-30 21:03:54 -0500

    added build

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 global.shopItems = require('./shopItems')();
 const cart = require('./cart');
 const characters = require('./characters');
-app.use(express.static('dist'));
+app.use(express.static('dist/angular-jurassic-park'));
 
 // For parsing application/json
 app.use(express.json());
